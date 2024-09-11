b6c47e976e9e3ccb761a5a9a488303d52dc27459
sandhya1102 <sandhay165@gmail.com>
2024-09-11 16:56:31 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//  function showsection(sectionId){
+//      var sections=document.querySelectorAll('.section');
+//      var navlinks=document.querySelectorAll(".nav-link")
+//      sections.forEach(showsection){
+//          if(sections===sectionId){
+//              sections.classList.add('show');
+//              sections.classList.remove('hide');
+//          }else{
+//              sections.classList.remove('show');
+//              sections.classList.add('hide');
+//          }
+//     };
+ 
+//      navlinks.forEach(showsection){
+//          if(link.getAttribute('onclick').includes(sectionId)){
+//              link.classList.add('active');
+//          }else{
+//              link.classList.remove('active');
+//          }
+//      }
+//  };
+
+// console.log("hii");
